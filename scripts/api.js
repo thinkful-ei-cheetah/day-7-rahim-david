5e2aa20f4927af39d3ac2ee61f0f6b31e0a8dcf0
rahmor <rahmor@yahoo.com>
2019-04-03 12:21:08 -0400

    ex6

--- scripts/api.js
+++ scripts/api.js
@@ -6,11 +6,22 @@ let api = (function() {
   function getItems() {
     return fetch(`${BASE_URL}/items`);
   }
-  
 
-  function createItem(name){
-    let newItem = JSON.stringify({name});
-    return fetch(`${BASE_URL}/items`, { 'method': 'POST', 'headers': {'Content-Type': 'application/json'}, 'body': newItem});
-  } 
-  return { createItem, getItems };
+  function createItem(name) {
+    let newItem = JSON.stringify({ name });
+    return fetch(`${BASE_URL}/items`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: newItem
+    });
+  }
+
+  function updateItem(id, updateData) {
+    return fetch(`${BASE_URL}/items/${id}`, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updateData)
+    });
+  }
+  return { createItem, getItems, updateItem };
 })();
--- scripts/index.js
+++ scripts/index.js
@@ -9,11 +9,13 @@ $(document).ready(function() {
 
 api
   .getItems()
-  .then(response => response.json())
+  .then(res => res.json())
   .then(items => {
-    items.forEach(item => store.addItem(item));
-    shoppingList.render();
-  });
+    const item = items[0];
+    return api.updateItem(item.id, { name: 'foobar' });
+  })
+  .then(res => res.json())
+  .then(() => console.log('updated!'));
 
 console.log(api.BASE_URL);
 
